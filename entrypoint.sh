f563e1c76676f9b9e9e56d6b7ce8eabd788edb19
SOOS-JAlvarez <jalvarez@soos.io>
2023-11-29 11:24:03 -0300

    PA-12057 & PA-12055 fix spaces on other options and verbose

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,23 +8,22 @@ SOOS_INTEGRATION_TYPE="Plugin"
 SOOS_BRANCH_NAME=${SOOS_BRANCH_NAME:-${GITHUB_REF}}
 
 PARAMS=(
-    "--apiKey ${SOOS_API_KEY}"
-    "--apiURL ${SOOS_API_URL}"
-    "--appVersion ${SOOS_APP_VERSION}"
-    "--branchName ${SOOS_BRANCH_NAME}"
+    "--apiKey" "${SOOS_API_KEY}"
+    "--apiURL" "${SOOS_API_URL}"
+    "--appVersion" "${SOOS_APP_VERSION}"
+    "--branchName" "${SOOS_BRANCH_NAME}"
     ${SOOS_BRANCH_URI:+--branchUri ${SOOS_BRANCH_URI}}
     ${SOOS_BUILD_URI:+--buildUri ${SOOS_BUILD_URI}}
     ${SOOS_BUILD_VERSION:+--buildVersion ${SOOS_BUILD_VERSION}}
-    "--clientId ${SOOS_CLIENT_ID}"
-    "--commitHash ${GITHUB_SHA}"
-    "--integrationName ${SOOS_INTEGRATION_NAME}"
-    "--integrationType ${SOOS_INTEGRATION_TYPE}"
+    "--clientId" "${SOOS_CLIENT_ID}"
+    "--commitHash" "${GITHUB_SHA}"
+    "--integrationName" "${SOOS_INTEGRATION_NAME}"
+    "--integrationType" "${SOOS_INTEGRATION_TYPE}"
     ${SOOS_OTHER_OPTIONS:+--otherOptions ${SOOS_OTHER_OPTIONS}}
-    "--onFailure ${SOOS_ON_FAILURE}"
-    "--projectName ${SOOS_PROJECT_NAME}"
-    ${SOOS_VERBOSE:+--verbose}
+    "--onFailure" "${SOOS_ON_FAILURE}"
+    "--projectName" "${SOOS_PROJECT_NAME}"
 )
 
-PARAMS_STRING="${PARAMS[@]}"
+[ "$SOOS_VERBOSE" == "true" ] && PARAMS+=("--verbose")
 
-node /usr/src/app/dist/index.js "${SOOS_TARGET_IMAGE}" ${PARAMS_STRING}
+node /usr/src/app/dist/index.js "${SOOS_TARGET_IMAGE}" "${PARAMS[@]}"
